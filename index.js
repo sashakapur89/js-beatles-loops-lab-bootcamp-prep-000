394cba973cd10007e758d5b3e313a5c167f2cb0d
Sasha Kapur <sashakapur89@gmail.com>
2018-02-07 03:55:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function theBeatlesPlay(musicians)(instruments){
+function theBeatlesPlay(musicians, instruments){
   var newArray = {};
   for (let counter = 0, counter<musicians.length, counter++){
-    if (counter===0){
       newArray.push[musicians[counter]+"plays"+instruments[counter]]
     }
-    else(){
-      
-    }
   }
-}
