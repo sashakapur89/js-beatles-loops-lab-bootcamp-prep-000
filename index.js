3f63b1b83d1cd3c0c72f4c90d15571b567cc3f8f
Sasha Kapur <sashakapur89@gmail.com>
2018-02-07 04:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function theBeatlesPlay(musicians, instruments){
   function iLoveTheBeatles(n){
     var newArray = [];
     do{
-      newArray.push['I love the Beatles!']
-      n++
+      newArray.push['I love the Beatles!'];
+      n++;
     }
-    while(n<15)
-    return newArray
+    while(n<15);
+    return newArray;
   }
